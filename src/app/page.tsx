d08a3279f30091c976cee5ee3e76624e3cfae288
seikou <seikou615@gmail.com>
2023-07-27 07:45:45 +0100

    Refactored with `BackgroundImage`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+// "use client"
+
+import { BackgroundAnimation } from "components"
+import Image from "next/image"
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <BackgroundAnimation />
       <h1>Home Page</h1>
     </main>
   )
