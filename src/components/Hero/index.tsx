ad00face58538436a7af51ae5f82d5b972e544d6
seikou <seikou615@gmail.com>
2023-07-29 00:36:31 +0100

    Refactored

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -16,7 +16,7 @@ function Hero() {
         <p>Hello, I'm Seikou 👋🏻</p>
         <h2>A {text} Developer</h2>
         <p>
-        —with a passion for crafting captivating experiences and a
+          with a passion for crafting captivating experiences and a
           commitment to embracing new technologies.
         </p>
         <button className={styles.button}>Portfolio</button>
