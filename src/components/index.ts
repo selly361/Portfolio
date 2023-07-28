7c123c798d966f6d971ea8553798b9aa96710a81
seikou <seikou615@gmail.com>
2023-07-29 00:25:59 +0100

    Exported default as `Header`/`Hero`

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
-export { default as BackgroundAnimation } from "./BackgroundAnimation"
+export { default as BackgroundAnimation } from "./BackgroundAnimation";
+export { default as Header } from "./Header";
+export { default as Hero } from "./Hero";
